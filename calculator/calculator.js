8cc322ebf430f2b7cbd7c1449b693bbf617c9df5
vinyavkrishnan <145954533+vinyavkrishnan@users.noreply.github.com>
2023-09-25 10:38:40 +0530

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,19 @@
+let result=document.getElementById("inputext");
+let calculate=(number)=>{
+    result.value+=number;
+
+}
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert("enter the valid result");
+    }
+}
+function clr(){
+    result.value="";
+}
+function del(){
+    result.value=result.value.slice(0,-1);
+}
